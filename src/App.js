245252ab8346ec7a5d8a7aab5f871cee62f76abf
Alex Brown <alexanderbrown.info@gmail.com>
2017-02-19 15:00:52 +0100

    created solution

--- src/App.js
+++ src/App.js
@@ -4,7 +4,13 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return <div>You have five minutes to create an awesome React app... Go!</div>;
+    return (
+      <div>
+        <h1>Awesome React App</h1>
+        <div><img src="https://3b0ad08da0832cf37cf5-435f6e4c96078b01f281ebf62986b022.ssl.cf3.rackcdn.com/articles/content/Bringing-Your-Kitten-Home_f968d7e4-043a-4260-b717-d88bd8ab3b6c.jpg"/></div>
+        <p>Every great app requires kittens, right?</p>
+      </div>
+    );
   }
 }
 
