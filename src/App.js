628848018851af623964788f7dcc2fe0b3324063
Alex Brown <alexanderbrown.info@gmail.com>
2017-02-19 14:49:52 +0100

    initial app creation

--- src/App.js
+++ src/App.js
@@ -4,17 +4,7 @@ import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-        <div className="App-header">
-          <img className="App-logo" src={logo} alt="React Logo"/>
-          <h1>Hello! Welcome to React.js!</h1>
-        </div>
-        <div className="App-intro">
-          You have five minutes to create an awesome React app... Go!
-        </div>
-      </div>
-    );
+    return <div>You have five minutes to create an awesome React app... Go!</div>;
   }
 }
 
